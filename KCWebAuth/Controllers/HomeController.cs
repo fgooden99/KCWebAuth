2678d12f7bcdbb441dd15af5ad41a5568a82bbf9
fgooden99 <fletcherdragon123@gmail.com>
2020-04-22 10:53:25 -0500

    Add project files.

---
+++ KCWebAuth/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KCWebAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[Authorize(Users = @"LarryK321@gmail.com")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //[Authorize(Users = @"StevenW123@gmail.com")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        //[Authorize(Users = @"LarryK321@gmail.com")]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}

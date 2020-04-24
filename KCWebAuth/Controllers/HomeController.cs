bf0777b5315582656ccab7e51d8645bad2544ab9
fgooden99 <fletcherdragon123@gmail.com>
2020-04-24 13:12:10 -0500

    Home changes

--- KCWebAuth/Controllers/HomeController.cs
+++ KCWebAuth/Controllers/HomeController.cs
@@ -8,19 +8,19 @@ namespace KCWebAuth.Controllers
 {
     public class HomeController : Controller
     {
-        //[Authorize(Users = @"LarryK321@gmail.com")]
+        [Authorize(Users = @"LarryK321@gmail.com")]
         public ActionResult Index()
         {
             return View();
         }
-        //[Authorize(Users = @"StevenW123@gmail.com")]
+        [Authorize(Users = @"StevenW123@gmail.com")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-        //[Authorize(Users = @"LarryK321@gmail.com")]
+        [Authorize(Users = @"LarryK321@gmail.com")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
